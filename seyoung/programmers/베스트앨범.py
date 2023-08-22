05d674297c821380243def4c1aa4f929e9584b6c
Seyoung Goo <0917sy@gmail.com>
2023-08-22 21:15:12 +0900

    approve: [PROGRAMMERS] 베스트앨범

--- seyoung/programmers/베스트앨범.py
+++ seyoung/programmers/베스트앨범.py
@@ -4,6 +4,23 @@ link: https://school.programmers.co.kr/learn/courses/30/lessons/42579
 key: 해시
 """
 from collections import defaultdict
+import heapq
+
+
+def solution(genres, plays):
+    answer = []
+
+    genre_dict = defaultdict(list)
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
+        genre_dict[genre].append((idx, play))
+
+    sorted_genres = sorted(genre_dict.items(), key=lambda x: sum([p for (_, p) in x[1]]), reverse=True)
+    for _, play_list in sorted_genres:
+        two_largest = heapq.nlargest(2, play_list, key=lambda x: (x[1], -x[0]))
+        for idx, _ in two_largest:
+            answer.append(idx)
+
+    return answer
 
 
 def solution(genres, plays):
