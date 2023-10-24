c039c405c920051bfc49f99f502d2d2af0e5f2cc
Seyoung Goo <0917sy@gmail.com>
2023-10-24 19:39:20 +0900

    approve: [BOJ] 2133. 타일 채우기

--- seyoung/beakjoon/2133_타일채우기/b2133.py
+++ seyoung/beakjoon/2133_타일채우기/b2133.py
@@ -13,10 +13,10 @@ def solution():
     dp = [0] * (n + 1)
     dp[0] = 1
     dp[2] = 3
+    prev_sum = 1
     for i in range(4, n + 1, 2):
-        dp[i] = dp[i - 2] * dp[2]
-        for j in range(i - 4, -1, -2):
-            dp[i] += dp[j] * 2
+        dp[i] = dp[i - 2] * dp[2] + prev_sum * 2
+        prev_sum += dp[i - 2]
 
     return dp[n]
 
